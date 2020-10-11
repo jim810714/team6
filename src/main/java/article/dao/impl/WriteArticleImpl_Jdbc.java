e24d0e0df8e28db13c374fb50ad985c7672ba6c8
x3765 <x3765@DESKTOP-MFP62VC>
2020-10-11 20:32:33 +0800

    jdbc file basic func done.

--- src/main/java/article/dao/impl/WriteArticleImpl_Jdbc.java
+++ src/main/java/article/dao/impl/WriteArticleImpl_Jdbc.java
@@ -126,7 +126,47 @@ public class WriteArticleImpl_Jdbc implements Serializable, ArticleDao {
 		}	
  
 		
+		// 修改一筆記錄---  TODO
 		
+			public int updateArticle(ArticleBean ab) {
+				int n = 0;
+
+//				String sql = "INSERT INTO Article " 
+//						+ " (id, title, ACTIVITYS_ID, SHOW_ARTICLE, "
+//						+ " MEMBER_ID, articletypes_id) " 
+//						+ " VALUES (?, ?, ?, ?, ?, ?)";
+				
+				String sql = "UPDATE Article SET" 
+						+ " (title=?, ACTIVITYS_ID=?,"
+						+ " VALUES (?, ?)";
+
+				try (
+					Connection connection = ds.getConnection();
+					PreparedStatement pStmt = connection.prepareStatement(sql);
+				) {
+//					pStmt.setInt(1, ab.getId());
+//					pStmt.setString(2, ab.getTitle());
+//					pStmt.setInt(3, ab.getActivitysId());
+//					pStmt.setInt(4, ab.getShowArticle());
+//					pStmt.setInt(5, ab.getMemberId());
+//					pStmt.setInt(6, ab.getArticleType());
+					
+					
+					pStmt.setString(1, ab.getTitle());
+					pStmt.setInt(2, ab.getActivitysId());
+
+					
+									
+					n = pStmt.executeUpdate();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+					throw new RuntimeException("WriteArticleImpl_Jdbc()#addArticle()發生例外: " 
+												+ ex.getMessage());
+				}finally {
+					
+				}
+				return n;
+			}	
 		
 		
 		
